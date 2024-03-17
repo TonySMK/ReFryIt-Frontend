34821b02cf8f2a5d883b6159a850968c3858d41c
TonySMK <tonysun25light@live.ca>
2024-03-17 15:09:33 -0400

    Initiated setting html of highlight cards and mapping

--- src/components/HighlightCard/HighlightCard.jsx
+++ src/components/HighlightCard/HighlightCard.jsx
@@ -1,5 +1,56 @@
 import "./HighlightCardStyles.scss";
+import copyIcon from "../../assets/icons/copy-icon-512.png";
+import closeIcon from "../../assets/icons/close-24px.svg";
 
-export default function HighlightCard() {
-  return <></>;
+export default function HighlightCard({ highlightInfo }) {
+  console.log(highlightInfo);
+  return (
+    <article className="highlightcardwrapper">
+      <section className="highlightcardwrapper__top">
+        <div className="title">{highlightInfo.title}</div>
+        <div className="urlanddatewrapper">
+          <div className="urllink">
+            <button className="urllink__buttoncopy buttonhightlightwrapper">
+              <img className="urllink__buttoncopy__icon" src={copyIcon} />
+            </button>
+
+            <div className="urllink__text">
+              {highlightInfo.domain + highlightInfo.domain_path}
+            </div>
+          </div>
+          <div className="dateadded">{highlightInfo.created_at}</div>
+        </div>
+      </section>
+      <section className="highlightcardwrapper__bottom">
+        <button className="deletehiglightbutton buttonhightlightwrapper">
+          <img className="deletehiglightbutton__icon" src={closeIcon} />
+        </button>
+        <main className="highlightinnerwrapper">
+          <button className="highlightinnerwrapper__webpageiconbutton buttonhightlightwrapper">
+            @
+            {/* <img className="highlightinnerwrapper__webpageiconbutton__icon" /> */}
+          </button>
+          <button className="highlightinnerwrapper__expandnotesbutton buttonhightlightwrapper">
+            V
+            {/* <img className="highlightinnerwrapper__expandnotesbutton__icon" /> */}
+          </button>
+          <section className="highlightinnerwrapper__hightlightcontent">
+            {highlightInfo.highlight_passage}
+          </section>
+          <button className="highlightinnerwrapper__edithighlight buttonhightlightwrapper">
+            E
+            {/* <img className="highlightinnerwrapper__edithighlight__icon" /> */}
+          </button>
+          <button className="highlightinnerwrapper__starhighlightbutton buttonhightlightwrapper">
+            #
+            {/* <img className="highlightinnerwrapper__starhighlightbutton__icon" /> */}
+          </button>
+          <button className="highlightinnerwrapper__addnotesbutton buttonhightlightwrapper">
+            +
+            {/* <img className="highlightinnerwrapper__addnotesbutton__icon" /> */}
+          </button>
+        </main>
+      </section>
+    </article>
+  );
 }
--- src/pages/BasePage/BasePage.jsx
+++ src/pages/BasePage/BasePage.jsx
@@ -3,28 +3,65 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import HighlightHolder from "../../components/HighlightHolder/HighlightHolder";
 import GroupSideBar from "../../components/GroupSideBar/GroupSideBar";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 import { useParams, useLocation } from "react-router-dom";
 
 export default function BasePage({ pageURL }) {
+  const [compState, setCompState] = useState(true);
+  const [highlightObject, setHighlightObject] = useState("");
+
   const { groupID } = useParams();
-  const location = useLocation();
-  console.log(groupID);
+  // const location = useLocation();
+  const thePageAttribute = pageURL;
+  // console.log(groupID);
   // console.log(window.location.pathname);
-  console.log(location);
-  console.log(location.pathname);
-  function fetch(somePageAttribute) {}
+  // console.log(location);
+  // console.log(location.pathname);
+
+  function fetchHighlightData(somePageAttribute) {
+    let finalURL;
+    if (somePageAttribute === "favorite") {
+      finalURL = "http://localhost:8080/api/highlights/filter/favorite/";
+    } else if (somePageAttribute === "group") {
+      finalURL = `http://localhost:8080/api/highlights/filter/group/${groupID}`;
+    } else if (somePageAttribute === "recent") {
+      finalURL = `http://localhost:8080/api/highlights/filter/recent/`;
+    }
+
+    axios
+      .get(finalURL)
+      .then((res) => {
+        // console.log(res.data);
+        setHighlightObject(res.data);
+        setCompState(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    fetchHighlightData(thePageAttribute);
+  }, [pageURL, groupID]);
 
   return (
-    <main>
-      <Header />
-      <div>
-        <HighlightHolder />
-        <aside className="groupsidebarvisibilitywrapper">
-          <GroupSideBar />
-        </aside>
-      </div>
-      <Footer />
-    </main>
+    <>
+      {compState ? (
+        <section>Loading Data...</section>
+      ) : (
+        <main>
+          <Header />
+          <div>
+            <HighlightHolder object={highlightObject} />
+            <aside className="groupsidebarvisibilitywrapper">
+              <GroupSideBar />
+            </aside>
+          </div>
+          <Footer />
+        </main>
+      )}
+    </>
   );
 }
